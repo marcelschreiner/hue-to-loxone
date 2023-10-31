6ed94ea313d6eb190705bcd304224a606f0b1633
Marcel Schreiner <40523792+marcelschreiner@users.noreply.github.com>
2023-10-31 19:46:11 +0100

    Update get_api_key.py to satisfy pylint

--- get_api_key.py
+++ get_api_key.py
@@ -1,20 +1,22 @@
+"""A python script to get a hue api key from a Philips Hue bridge."""
 import asyncio
 from aiohue import create_app_key
 
 # Insert the ip address of you Philips Hue bridge
-hue_ip = "192.168.1.123"
+HUE_IP = "192.168.1.123"
 
 
 async def main():
-    print("Creating hue_app_key for bridge: ", hue_ip)
+    """Creates a hue api key, after pressing the link button on the bridge."""
+    print("Creating hue_app_key for bridge: ", HUE_IP)
     input("Press the link button on the bridge, then press enter to continue...")
 
     # request api_key from bridge
     try:
-        api_key = await create_app_key(hue_ip, "authentication_example")
+        api_key = await create_app_key(HUE_IP, "authentication_example")
         print("Authentication succeeded, api key: ", api_key)
         print("NOTE: Add hue_app_key in main.py for next connections, it does not expire.")
-    except Exception as exc:
+    except Exception as exc: # pylint: disable=broad-exception-caught
         print("ERROR: ", str(exc))
 
 
